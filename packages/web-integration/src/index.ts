1dc548ebb7c15441cea3c11d71edaa058040de3e
quanruzhuoxiu <quanruzhuoxiu@gmail.com>
2024-09-24 15:36:18 +0800

    fix: delete side effect

--- packages/web-integration/src/index.ts
+++ packages/web-integration/src/index.ts
@@ -1,5 +1,5 @@
-export { PlaywrightAiFixture } from './playwright';
-export type { PlayWrightAiFixtureType } from './playwright';
+// export { PlaywrightAiFixture } from './playwright';
+// export type { PlayWrightAiFixtureType } from './playwright';
 
 export { PuppeteerAgent } from './puppeteer';
 export { PlaywrightAgent } from './playwright';
--- packages/web-integration/src/playwright/index.ts
+++ packages/web-integration/src/playwright/index.ts
@@ -1,4 +1,4 @@
-export type { PlayWrightAiFixtureType } from './ai-fixture';
-export { PlaywrightAiFixture } from './ai-fixture';
+// export type { PlayWrightAiFixtureType } from './ai-fixture';
+// export { PlaywrightAiFixture } from './ai-fixture';
 export { PageAgent as PlaywrightAgent } from '@/common/agent';
 export { WebPage as PlaywrightWebPage } from './page';
